15527a40ffffd398268ad898b4f0da8ff0d9582b
Divya Prakash Tiwari <tdivyaprakash9971@gmail.com>
2025-03-18 19:02:21 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"  // treat all JS code as newer version !
+
+// alert(3+3)  // we are using nodejs, not browser 
+
+let name = "divya"
+let num = 12112
+let islogged_IN = false
+
+
+//boolean = true/false
+// bigint =  for big int values
+// null = standalone value (representation of an empty value)
+// undefined = variable is declared , but no value is declared it.\
+//symbol = unique
+
+//object
+
+console.log(typeof"divya")
+console.log(typeof num)
