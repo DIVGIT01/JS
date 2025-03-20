d2c855883b0c1dfa9c5a19b2b2eaa6667b8302be
Divya Prakash Tiwari <tdivyaprakash9971@gmail.com>
2025-03-20 15:45:29 +0000

    some dataypes notes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -17,3 +17,33 @@ let islogged_IN = false
 
 console.log(typeof"divya")
 console.log(typeof num)
+
+
+// ********** DATATYPES ***********
+
+
+//primitive
+
+// 7 types : STRING , NUMBER, NULL, BOOLEAN, UNDEFINED, SYMBOL, BIGINT
+
+// REFERENCE (non primitve)
+
+// Array, Objects , Functions
+
+const id = Symbol('123')
+const ANOTHERid = Symbol('123')
+
+console.log (id === ANOTHERid)                    // false
+
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+console.log("2"=== 2)              // it will check the datatypes , then compare !
