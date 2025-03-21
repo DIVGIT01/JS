8268bffeb456342d54d1226187ee777838a29537
Divya Prakash Tiwari <tdivyaprakash9971@gmail.com>
2025-03-21 16:30:59 +0000

    memory(heap stack)

---
+++ 01_basics/memory.js
@@ -0,0 +1,19 @@
+// STACK MEMORY - It is for the Primitve datatypes.
+//                It'll give the copy of the original values. so the changes dont be happen in the OG value. whereas,
+
+// HEAP Memory - It is for the Non - primitive datatypes.
+//               It'll give the reference(address) of the OG value. so the changes done in the OG values. 
+
+
+let myName = "divyaprakashtiwari"
+let hey = myName;
+
+console.log(hey);
+
+
+let amn = {
+    myEmail :"dpt@gmail.com"
+    
+}
+
+console.log(amn);
