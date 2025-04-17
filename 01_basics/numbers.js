06f2f306ae2d40734067d56aaddbcf37afc469e5
Divya Prakash Tiwari <tdivyaprakash9971@gmail.com>
2025-04-17 16:51:36 +0000

    math

--- 01_basics/numbers.js
+++ 01_basics/numbers.js
@@ -1,4 +1,4 @@
-const score = 400;
+/* const score = 400;
 console.log(score);
 
 const balance = new Number(100);
@@ -8,9 +8,23 @@ console.log(balance.toString().length);         // first converts it to string ,
 console.log(balance.toFixed(1));                  // 100.0
 
 /*const otherNumber = 23.869
-console.log(otherNumber.toPrecision(3));            // 23.9*/
+console.log(otherNumber.toPrecision(3));            // 23.9
 const otherNumber = 123.8966
 console.log(otherNumber.toPrecision(3));            // 124
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'))         // 10,00,000
+console.log(hundreds.toLocaleString('en-IN'))         // 10,00,000  */
+
+
+
+// *************************** MATHS ****************************
+
+console.log(Math)
+console.log(Math.abs(-4));            // removes negative sign
+console.log(Math.round(4.6));         // gives round off
+console.log(Math.ceil(4.2));          // gives next highest number agr 0.1 bhi aage hua to !
+console.log(Math.floor(4.9));         // just  opposite of the floor
+console.log(Math.min(4,3,6,8));       // gives the minimal number !
+console.log(Math.max(4,3,6,8));        // gives the maximum number !
+
+console.log(Math.random());
