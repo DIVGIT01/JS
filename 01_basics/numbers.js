6db0a1236fc4928baccc0f6bfb0b6b78ba917f76
Divya Prakash Tiwari <tdivyaprakash9971@gmail.com>
2025-04-18 08:39:03 +0000

    maths

--- 01_basics/numbers.js
+++ 01_basics/numbers.js
@@ -20,11 +20,18 @@ console.log(hundreds.toLocaleString('en-IN'))         // 10,00,000  */
 // *************************** MATHS ****************************
 
 console.log(Math)
-console.log(Math.abs(-4));            // removes negative sign
-console.log(Math.round(4.6));         // gives round off
-console.log(Math.ceil(4.2));          // gives next highest number agr 0.1 bhi aage hua to !
-console.log(Math.floor(4.9));         // just  opposite of the floor
+console.log(Math.abs(-4));            // removes negative sign   (4)
+console.log(Math.round(4.6));         // gives round off   (5)
+console.log(Math.ceil(4.2));          // gives next highest number agr 0.1 bhi aage hua to !    (5)
+console.log(Math.floor(4.9));         // just  opposite of the floor          (4)
 console.log(Math.min(4,3,6,8));       // gives the minimal number !
 console.log(Math.max(4,3,6,8));        // gives the maximum number !
 
 console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10)+ 1);
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
