05d43ebadd8d32f3c177f31e3fa54cd06fd00ba5
Divya Prakash Tiwari <tdivyaprakash9971@gmail.com>
2025-04-19 16:52:01 +0000

    date

---
+++ 01_basics/date_time.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+console.log(myDate.tostring());
+console.log(myDate.toDateString);
+console.log(myDate.toLocalestring());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2025, 3, 19);
+console.log(myCreatedDate.toDateString())
+
+
