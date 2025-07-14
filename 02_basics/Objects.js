8524379c01b042f0a0f77646d7c2eaf495c3e45b
Divya Prakash Tiwari <tdivyaprakash9971@gmail.com>
2025-07-14 19:00:04 +0000

    functions in object

--- 02_basics/Objects.js
+++ 02_basics/Objects.js
@@ -7,7 +7,7 @@ const mySum = Symbol("key1");
 
 const JsUser = {
     name : "Divya",
-    "full name" : "DIVYA",
+    "full name" : "DIVYA",     // ----we cant access this with dot. we can omly access it by using square brackets.
     [mySum] : "mykey1",       // how we declare symbol as key pair format.
     age : 20,
     email : "tdivyaprakash9971@gmail.com",
@@ -15,3 +15,27 @@ const JsUser = {
 }
 
 console.log(JsUser)
+console.log(JsUser["full name"])
+
+//CHANGING VALUES :-
+
+/*JsUser.email = "tdivyachat@gmail.com"
+console.log(JsUser)
+
+// FREEZING THE VALUE :- No change will happen in the values of object.
+Object.freeze(JsUser)
+JsUser.email = "hariomgpt@gmail.com"
+console.log(JsUser)*/
+
+JsUser.greeting = function(){
+    console.log("Hello JS User")
+}
+
+JsUser.greeting1 = function(){
+    console.log(`Hello JS User, ${this.name}`)
+}
+
+
+console.log (JsUser.greeting())
+console.log(JsUser.greeting1())
+
